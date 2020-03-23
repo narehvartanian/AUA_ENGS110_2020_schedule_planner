756bf4b0872085ab107ea8a5d88108ff049c9ff9
narehvartanian <61308553+narehvartanian@users.noreply.github.com>
2020-03-23 16:35:55 +0400

    Add files via upload

---
+++ Initialize schedule.py
@@ -0,0 +1,11 @@
+import json
+with open ("schedule.json") as jsonFile:
+ data = json.load(jsonFile)
+jsonFile.close()
+for day, activity in data.items():
+    for activity, time in activity.items():
+        print("How much is your " + activity + " time for " + day + "?")
+        data[day][activity] = int(input())
+        jsonFile = open('schedule.json', 'w')
+        json.dump(data, jsonFile)
+        jsonFile.close()
