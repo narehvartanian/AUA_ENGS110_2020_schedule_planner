abf40e02d415bd3066cd4cf16842c2da1a18ab76
narehvartanian <61308553+narehvartanian@users.noreply.github.com>
2020-03-24 14:52:52 +0400

    the case when user inserts wrong number added!

--- Initialize schedule(first story).py
+++ Initialize schedule(first story).py
@@ -1,12 +1,28 @@
 import json
-with open ("schedule.json") as jsonFile:
- data = json.load(jsonFile)
+with open("schedule1.json") as jsonFile:
+    data = json.load(jsonFile)
 jsonFile.close()
 for day, activity in data.items():
-    for activity, time in activity.items():
-        print("How much is your " + activity + " time for " + day + "?")
-        data[day][activity] = int(input())
-        jsonFile = open('schedule.json', 'w', indent =2)
-        json.dump(data, jsonFile)
-        jsonFile.close()
-        
+    weekCheck = False
+    while (weekCheck == False):
+        total = 0
+        for activity, time in activity.items():
+            dayCheck = False
+            while (dayCheck != True):
+                print("How much is your " + activity + " time for " + day + "?")
+                userInput = int(input())
+                if(userInput < 24 and userInput >= 0):
+                    data[day][activity] = userInput
+                    total += userInput
+                    dayCheck = True
+                else:
+                    print("Your inserted hour is invalid, please insert a number less than 24.")
+        if (total < 24):
+            weekCheck = True
+            jsonFile = open("schedule1.json", "w")
+            json.dump(data, jsonFile, indent=2)
+            jsonFile.close()
+        else:
+            print("Error! Your activity time for today is more than 24 hours!")
+            print("Please reschedule your " + day + " activites")
+            activity = data[day]
